3f8892bffbc573d4412ef7197614c3abffa291e1
Victor Ozoterio <victorozoterio@gmail.com>
2024-08-23 13:30:19 -0300

    feat: persistindo dados através de um arquivo físico JSON

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
 
-const database = [];
+const database = new Database();
 
 export const routes = [
 	{
 		method: "GET",
 		path: "/tasks",
 		handler: (req, res) => {
-			return res.end(JSON.stringify(database));
+			const tasks = database.select("tasks");
+			return res.end(JSON.stringify(tasks));
 		},
 	},
 	{
@@ -16,18 +18,18 @@ export const routes = [
 		handler: (req, res) => {
 			const { title, description } = req.body;
 
-			database.push({
+			const task = {
 				id: randomUUID(),
 				title,
 				description,
 				completed_at: null,
 				created_at: new Date(),
 				updated_at: new Date(),
-			});
+			};
 
-			return res
-				.writeHead(201)
-				.end(JSON.stringify(database[database.length - 1]));
+			database.insert("tasks", task);
+
+			return res.writeHead(201).end(JSON.stringify(task));
 		},
 	},
 ];
